d611f0444a80451f4ce01e019fce9e43db5b6e19
solarlime <solarlime@outlook.com>
2020-06-14 16:56:39 +0300

    Created a class for Drag and Drop + added chooseItem method

--- src/js/moveItems.js
+++ src/js/moveItems.js
@@ -16,60 +16,82 @@ function changeOrder(items, targetItem, notMovingItemOrder) {
     });
 }
 
-export default function moveItems(event, column, drag, oldColumn) {
-  // Элемент, на который навели ячейку
-  const firstMovingItem = document.elementsFromPoint(event.clientX, event.clientY)
-    .find((item) => item.classList.contains('column-item')
-      && item.getAttribute('data-id') !== drag.getAttribute('data-id'));
-  const items = Storage.getItems();
-  // Указатель на перетаскиваемый элемент в localStorage
-  const targetItem = items.find((item) => item.id.toString() === drag.getAttribute('data-id'));
-  // Новое значение для колонки
-  targetItem.column = column.id;
-  // Фиксируем начальные значения ячейки.
-  // Важно сохранить order, чтобы отфильтровать лишнее в localStorage
-  const notMovingItemOrder = parseInt({ ...targetItem }.order, 10);
-  // Вариант 1: колонка не изменилась
-  if (oldColumn.id === targetItem.column) {
-    // Вариант 1.1: колонка не изменилась, навели на другую ячейку
-    if (firstMovingItem) {
-      // Ячейке присваиваем (номер элемента - 1), на который навели
-      targetItem.order = parseInt(firstMovingItem.getAttribute('data-order'), 10) - 1;
-      changeOrder(items, targetItem, notMovingItemOrder);
-    } else {
-      // Вариант 1.2: колонка не изменилась, навели на пустое место
-      // Ячейку ставим последней посредством length
-      targetItem.order = items.filter((item) => item.column === targetItem.column).length;
-      changeOrder(items, targetItem, notMovingItemOrder);
-    }
-  } else {
-    // Вариант 2: колонка изменилась
-    // Заполняем пустоту в старой колонке
-    items.filter((item) => item.column === oldColumn.id).map((item) => {
-      if (parseInt(item.order, 10) >= notMovingItemOrder) {
-        item.order = parseInt(item.order, 10) - 1;
+export default class MoveItems {
+  static chooseItem(event, nullCoordinates) {
+    event.preventDefault();
+    if (event.target.classList.contains('column-item-title')) {
+      // Указатель на перетаскиваемый элемент в DOM
+      const drag = document.elementFromPoint(event.clientX, event.clientY).closest('li.column-item');
+      if (drag) {
+        drag.style.transform = 'rotate(2deg)';
+        // "Начало координат". Понадобится при Drag and Drop
+        nullCoordinates.x = event.clientX;
+        nullCoordinates.y = event.clientY;
+        drag.classList.add('drag');
+        drag.style.cursor = 'grabbing';
+        return drag;
       }
-      return item;
-    });
-    // Вариант 2.1: колонка изменилась, наводим на другую ячейку
-    if (firstMovingItem) {
-      // Ячейке присваиваем номер элемента, на который навели
-      // Всех, кто больше, увеличиваем на единицу
-      targetItem.order = parseInt(firstMovingItem.getAttribute('data-order'), 10);
-      items.filter((item) => item.column === targetItem.column
-        && item.id !== targetItem.id)
-        .map((item) => {
-          if (parseInt(item.order, 10) >= targetItem.order) {
-            item.order = parseInt(item.order, 10) + 1;
-          }
-          return item;
-        });
-    } else {
-      // Вариант 2.2: колонка изменилась, навели на пустое место
-      // Ячейку ставим последней посредством length
-      targetItem.order = items.filter((item) => item.column === targetItem.column).length;
     }
+    return null;
   }
-  Storage.setItems(items);
-  App.update();
+
+  static putItem(event, column, drag) {
+    // Фиксируем старую колонку, понадобится при перестроении колонок
+    const oldColumn = drag.closest('.column-container');
+    // Элемент, на который навели ячейку
+    const firstMovingItem = document.elementsFromPoint(event.clientX, event.clientY)
+      .find((item) => item.classList.contains('column-item')
+        && item.getAttribute('data-id') !== drag.getAttribute('data-id'));
+    const items = Storage.getItems();
+    // Указатель на перетаскиваемый элемент в localStorage
+    const targetItem = items.find((item) => item.id.toString() === drag.getAttribute('data-id'));
+    // Новое значение для колонки
+    targetItem.column = column.id;
+    // Фиксируем начальные значения ячейки.
+    // Важно сохранить order, чтобы отфильтровать лишнее в localStorage
+    const notMovingItemOrder = parseInt({ ...targetItem }.order, 10);
+    // Вариант 1: колонка не изменилась
+    if (oldColumn.id === targetItem.column) {
+      // Вариант 1.1: колонка не изменилась, навели на другую ячейку
+      if (firstMovingItem) {
+        // Ячейке присваиваем (номер элемента - 1), на который навели
+        targetItem.order = parseInt(firstMovingItem.getAttribute('data-order'), 10) - 1;
+        changeOrder(items, targetItem, notMovingItemOrder);
+      } else {
+        // Вариант 1.2: колонка не изменилась, навели на пустое место
+        // Ячейку ставим последней посредством length
+        targetItem.order = items.filter((item) => item.column === targetItem.column).length;
+        changeOrder(items, targetItem, notMovingItemOrder);
+      }
+    } else {
+      // Вариант 2: колонка изменилась
+      // Заполняем пустоту в старой колонке
+      items.filter((item) => item.column === oldColumn.id).map((item) => {
+        if (parseInt(item.order, 10) >= notMovingItemOrder) {
+          item.order = parseInt(item.order, 10) - 1;
+        }
+        return item;
+      });
+      // Вариант 2.1: колонка изменилась, наводим на другую ячейку
+      if (firstMovingItem) {
+        // Ячейке присваиваем номер элемента, на который навели
+        // Всех, кто больше, увеличиваем на единицу
+        targetItem.order = parseInt(firstMovingItem.getAttribute('data-order'), 10);
+        items.filter((item) => item.column === targetItem.column
+          && item.id !== targetItem.id)
+          .map((item) => {
+            if (parseInt(item.order, 10) >= targetItem.order) {
+              item.order = parseInt(item.order, 10) + 1;
+            }
+            return item;
+          });
+      } else {
+        // Вариант 2.2: колонка изменилась, навели на пустое место
+        // Ячейку ставим последней посредством length
+        targetItem.order = items.filter((item) => item.column === targetItem.column).length;
+      }
+    }
+    Storage.setItems(items);
+    App.update();
+  }
 }
