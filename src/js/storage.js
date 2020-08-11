f8e685a20c38c978d76ab86493b68e49d8457f14
solarlime <solarlime@outlook.com>
2020-08-11 07:48:45 +0300

    Added catching errors for localStorage (as I think, there must be only a QUOTA_EXCEEDED_ERROR)

--- src/js/storage.js
+++ src/js/storage.js
@@ -4,6 +4,10 @@ export default class Storage {
   }
 
   static setItems(items) {
-    localStorage.setItem('items', JSON.stringify(items));
+    try {
+      localStorage.setItem('items', JSON.stringify(items));
+    } catch (error) {
+      alert('Oops! The localStorage is almost full! Delete something to set new files.');
+    }
   }
 }
