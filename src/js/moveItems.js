db2e9756edfe24ddcd1b4e8bb91901add8b0226f
solarlime <solarlime@outlook.com>
2020-08-31 13:53:22 +0300

    Rewrote DnD again. I hope that now it's cleaner

--- src/js/moveItems.js
+++ src/js/moveItems.js
@@ -9,31 +9,31 @@ export default class MoveItems {
   static chooseItem(event, delta) {
     event.preventDefault();
     if (event.target.classList.contains('column-item-title')) {
-      const eventCoordinates = () => ((event.type === 'mousedown') ? event : event.changedTouches[0]);
+      const eventResolved = Utils.eventResolver(event);
       // Указатель на перетаскиваемый элемент в DOM
-      const drag = document.elementFromPoint(eventCoordinates().clientX, eventCoordinates().clientY).closest('li.column-item');
+      const drag = document.elementFromPoint(eventResolved.clientX, eventResolved.clientY).closest('li.column-item');
       if (drag) {
         drag.style.transform = 'rotate(2deg)';
-        delta.x = eventCoordinates().pageX - drag.getBoundingClientRect().left;
-        delta.y = eventCoordinates().pageY - drag.getBoundingClientRect().top;
+        delta.x = eventResolved.pageX - drag.getBoundingClientRect().left;
+        delta.y = eventResolved.pageY - drag.getBoundingClientRect().top;
         drag.classList.add('drag');
-        drag.style.left = `${eventCoordinates().pageX - delta.x}px`;
-        drag.style.top = `${eventCoordinates().pageY - delta.y}px`;
-        drag.style.cursor = 'grabbing';
+        drag.style.left = `${eventResolved.pageX - delta.x}px`;
+        drag.style.top = `${eventResolved.pageY - delta.y}px`;
+        drag.querySelector('.column-item-title').style.cursor = 'grabbing';
         return drag;
       }
     }
     return null;
   }
 
-  static dropItem(event, drag) {
+  static dropItem(event, drag, space) {
     if (drag) {
       // Новая колонка (если это именно она)
       const column = document
         .elementsFromPoint(event.clientX, event.clientY)
         .find((item) => item.classList.contains('column-container'));
       if (column) {
-        MoveItems.putItem(event, column, drag);
+        MoveItems.putItem(event, column, drag, space);
       } else {
         drag.style.transform = '';
         drag.classList.remove('drag');
@@ -46,28 +46,53 @@ export default class MoveItems {
     // Фиксируем старую колонку, понадобится при перестроении колонок
     const oldColumn = drag.closest('.column-container').id;
     const pointItem = document.elementsFromPoint(
-      Utils.eventCoordinates(event).clientX,
-      Utils.eventCoordinates(event).clientY,
+      Utils.eventResolver(event).clientX,
+      Utils.eventResolver(event).clientY,
     ).find(
-      (item) => item.classList.contains('column-item') && !item.classList.contains('drag'),
+      (item) => (item.classList.contains('column-item')
+        || item.classList.contains('column-space')) && !item.classList.contains('drag'),
     );
     const dragId = drag.getAttribute('data-id');
     // Новая (возможно) колонка
     const targetColumn = document.elementsFromPoint(event.clientX, event.clientY)
       .find((item) => item.classList.contains('column-container')).id;
     const items = Storage.getItems();
-    let endItem;
     // Новый номер. В зависимости от ситуации присваиваться будет по-разному
     let endOrder;
     const dragOrder = parseInt(drag.getAttribute('data-order'), 10);
+    // Определяем элемент, на котором произошло событие mousedown / touchend
+    // eslint-disable-next-line func-names
+    const endItem = (function resolveItem() {
+      // Навели выше середины ячейки
+      if (pointItem
+        && pointItem.previousElementSibling
+        && pointItem.previousElementSibling.classList.contains('column-space')) {
+        return pointItem;
+      }
+      // Навели ниже середины ячейки
+      if (pointItem
+        && pointItem.nextElementSibling
+        && pointItem.nextElementSibling.classList.contains('column-space')
+        // Предусматриваем, что ячейка не должна быть последней
+        && pointItem.nextElementSibling.nextElementSibling) {
+        return pointItem.nextElementSibling.nextElementSibling;
+      }
+      // Навели на заполнитель
+      if (pointItem
+        && pointItem.classList.contains('column-space')
+        && pointItem.nextElementSibling) {
+        return pointItem.nextElementSibling;
+      }
+      // Если последняя ячейка или пустое место
+      return null;
+    }());
+
     // Вариант 1. Колонка не меняется
     if (targetColumn === oldColumn) {
       // 1.1. Mouseup на пустом месте
-      if (!pointItem) {
+      if (!endItem) {
         endOrder = items.filter((item) => item.column === targetColumn).length;
-      //  1.2. Mouseup на одной из ячеек
       } else {
-        endItem = parseInt(pointItem.getAttribute('data-order'), 10) <= dragOrder ? pointItem : pointItem.previousElementSibling;
         endOrder = parseInt(endItem.getAttribute('data-order'), 10);
       }
       items.find((item) => item.id === dragId).order = endOrder;
@@ -101,11 +126,11 @@ export default class MoveItems {
         });
       const targetItem = items.find((item) => item.id === dragId);
       // 2.1. Mouseup на пустом месте
-      if (!pointItem) {
+      if (!endItem) {
         endOrder = items.filter((item) => item.column === targetColumn).length + 1;
       //  2.2 Mouseup на одной из ячеек
       } else {
-        endOrder = parseInt(pointItem.getAttribute('data-order'), 10);
+        endOrder = parseInt(endItem.getAttribute('data-order'), 10);
         // Ячейки с номером > endOrder увеличиваем на 1
         items.filter((item) => item.column === targetColumn
           && parseInt(item.order, 10) >= endOrder)
