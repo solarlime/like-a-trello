c82f6da60921d60d5e9198f55b78c7513fbd35bb
solarlime <solarlime@outlook.com>
2020-09-04 22:20:31 +0300

    Added a reset() method

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-cycle */
 import Storage from './storage';
 import Page from './page';
-import Modals from './modals';
 import Utils from './utils';
 
 export default class App {
@@ -11,8 +10,22 @@ export default class App {
     page.addListeners();
   }
 
+  static reset() {
+    document.forms['add-and-update'].reset();
+    Array.from(document.querySelector('.files').children).forEach((item) => item.remove());
+    const file = Array.from(document.querySelector('.modal-file-container').children)
+      .find((item) => item.classList.contains('preview') || item.classList.contains('preview-image'));
+    const space = document.querySelector('.column-space');
+    if (space) {
+      space.remove();
+    }
+    if (file) {
+      file.remove();
+    }
+  }
+
   static update() {
-    Modals.reset();
+    App.reset();
     document.querySelectorAll('li.column-item').forEach((item) => item.remove());
     if (Storage.getItems()) {
       Storage.getItems().sort((a, b) => a.order - b.order).forEach((item) => Utils.render(item));
