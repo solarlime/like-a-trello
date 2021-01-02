47f75f0c21f7b4065d6af8f8eb9234c5e1b8239e
solarlime <solarlime@outlook.com>
2021-01-02 22:20:07 +0300

    Changed modals.js for a server logic

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,12 +1,11 @@
-/* eslint-disable import/no-cycle */
+/* eslint-disable import/no-cycle, no-param-reassign */
 import id from 'uniqid';
 import Storage from './storage';
-import App from './app';
 import Utils from './utils';
 
 export default class Modals {
   // Функция отображения модельного окна
-  static show(modal, row, target, filesToSave) {
+  static show(modal, row, target, list, filesToSave) {
     // Если в ячейке файл, помещаем копию контейнера в окно
     if (target && target.closest('li.file-element')) {
       const fileNode = target.closest('li.file-element').querySelector('.preview, .preview-image').cloneNode(true);
@@ -14,15 +13,14 @@ export default class Modals {
       modal.querySelector('.modal-file-container').insertAdjacentElement('afterbegin', fileNode);
     }
     if (target && target.classList.contains('new')) {
-      this.column = target.previousElementSibling.id;
+      this.column = target.closest('.column').querySelector('.column-container').id;
     }
     const input = row ? modal.querySelector('#description') : 0;
     if (row) {
       input.value = row.querySelector('.column-item-title').textContent;
     }
-    const items = Storage.getItems();
-    if (items && row) {
-      const filesToRender = items.find((item) => item.id === row.getAttribute('data-id')).files;
+    if (list && row) {
+      const filesToRender = list.find((item) => item.id === row.getAttribute('data-id')).files;
       // Отрисовываем файлы
       if (filesToRender && filesToRender.length) {
         filesToRender.forEach(
@@ -52,50 +50,36 @@ export default class Modals {
   }
 
   // Функция сохранения: отправка данных в localStorage
-  static save(modal, button, column, row, filesToSave) {
+  static async save(modal, button, column, rowId, filesToSave, list, quota) {
     const name = modal.querySelector('#description').value.trim();
-    let data = Storage.getItems();
-    if (row) {
-      const target = data.find((item) => item.id.toString() === row.getAttribute('data-id'));
+    if (rowId) {
+      const target = list.find((item) => item.id.toString() === rowId);
       target.name = name;
       target.files = filesToSave;
     } else {
-      if (!data) {
-        data = [];
+      if (!list) {
+        list = [];
       }
-      const order = () => (Storage.getItems() ? Storage.getItems()
-        .filter((item) => item.column === column).length + 1 : 1);
-      data.push({
-        id: id(),
-        order: order(),
-        column,
-        name,
-        files: filesToSave,
-      });
+      const order = () => (list ? list.filter((item) => item.column === column).length + 1 : 1);
+      const newbie = {
+        id: id(), order: order(), column, name, files: filesToSave,
+      };
+      list.push(newbie);
+      await Storage.request('new', JSON.stringify(newbie));
     }
-    Storage.setItems(data);
-    App.update();
     return Modals.cancel();
   }
 
   // Функция удаления ячейки
-  static delete(row) {
-    const data = () => Storage.getItems().filter((item) => item.id.toString() !== row.getAttribute('data-id'))
-      .map((item) => {
-        if (item.column === row.closest('.column-container').id && parseInt(item.order, 10) > parseInt(row.getAttribute('data-order'), 10)) {
-          // eslint-disable-next-line no-param-reassign
-          item.order = parseInt(item.order, 10) - 1;
-        }
-        return item;
-      });
-    Storage.setItems(data());
-    App.update();
+  static async delete(rowId, list) {
+    list.splice(list.findIndex((item) => item.id === rowId), 1);
+    await Storage.request('delete', JSON.stringify({ id: rowId }));
     Modals.cancel();
   }
 
   // Функция закрытия окна
   static cancel() {
-    App.reset();
+    Utils.reset();
     document.querySelector('#description').style.borderColor = '';
     document.querySelector('button.save').disabled = true;
     document.querySelectorAll('.error').forEach((message) => message.classList.add('hidden'));
