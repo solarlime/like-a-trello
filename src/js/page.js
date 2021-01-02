f8c8464bc3e78199ba4d6e116e7f22c95db3860e
solarlime <solarlime@outlook.com>
2021-01-02 22:06:12 +0300

    Bugfix: 100-vh problem for mobile devices was fixed (I hope so)

--- src/js/page.js
+++ src/js/page.js
@@ -57,6 +57,16 @@ export default class Page {
       return 1;
     }
 
+    // Обработчик изменения размера экрана. В какой-то мере решает проблему 100vh.
+    // Спасибо Louis Hoebregts (@mamboleoo) за идею
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    window.dispatchEvent(new Event('resize'));
+    window.oncontextmenu = () => false;
+
     // Обработчики для кнопок 'Add new item'
     this.pluses.forEach((plus) => {
       plus.addEventListener('touchend', (event) => {
